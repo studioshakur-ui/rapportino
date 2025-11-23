36b664eb5686c4a5dfa742a3a3ff839a0ffc37e2
studioshakur-ui <studio.shakur@gmail.com>
2025-11-23 13:47:18 +0100

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,19 +3,6 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
-// ✅ enregistre le SW en prod
-import { registerSW } from 'virtual:pwa-register'
-
-registerSW({
-  onNeedRefresh() {
-    // micro-toast simple (pas intrusif)
-    console.log('[PWA] Update disponible, recharge la page.')
-  },
-  onOfflineReady() {
-    console.log('[PWA] App prête offline.')
-  }
-})
-
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
