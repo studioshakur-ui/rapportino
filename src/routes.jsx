0f4a202c03cf51fb4f44080d48357f39aa0754f7
studioshakur-ui <studio.shakur@gmail.com>
2025-11-25 21:08:13 +0100

    Update routes.jsx

--- src/routes.jsx
+++ src/routes.jsx
@@ -1,48 +1,33 @@
-import React from 'react'
-import { Routes, Route } from 'react-router-dom'
+// Dashboard CAPO
+<Route
+  path="/*"
+  element={
+    <RequireRole allow={['CAPO']}>
+      <AppShell />
+    </RequireRole>
+  }
+/>
 
-import RequireRole from './auth/RequireRole'
-import Login from './pages/Login'
+//
+// UFFICIO
+//
+<Route
+  path="/ufficio/*"
+  element={
+    <RequireRole allow={['UFFICIO']}>
+      <UfficioShell />
+    </RequireRole>
+  }
+/>
 
-import AppShell from './AppShell'
-import UfficioShell from './UfficioShell'
-import DirectionShell from './DirectionShell'
-
-export default function AppRoutes() {
-  return (
-    <Routes>
-      {/* Public */}
-      <Route path="/login" element={<Login />} />
-
-      {/* CAPO */}
-      <Route
-        path="/*"
-        element={
-          <RequireRole allow={['CAPO']}>
-            <AppShell />
-          </RequireRole>
-        }
-      />
-
-      {/* UFFICIO */}
-      <Route
-        path="/ufficio/*"
-        element={
-          <RequireRole allow={['UFFICIO']}>
-            <UfficioShell />
-          </RequireRole>
-        }
-      />
-
-      {/* DIREZIONE */}
-      <Route
-        path="/direction/*"
-        element={
-          <RequireRole allow={['DIREZIONE']}>
-            <DirectionShell />
-          </RequireRole>
-        }
-      />
-    </Routes>
-  )
-}
+//
+// DIREZIONE
+//
+<Route
+  path="/direction/*"
+  element={
+    <RequireRole allow={['DIREZIONE']}>
+      <DirectionShell />
+    </RequireRole>
+  }
+/>
