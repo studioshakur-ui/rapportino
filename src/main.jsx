088be7f4569d153b9d3c775e3421e162475f40d6
studioshakur-ui <studio.shakur@gmail.com>
2025-11-26 21:34:00 +0100

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,10 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
-import AppRoutes from './routes'
-import { AuthProvider } from './auth/AuthProvider'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <AuthProvider>
-        <AppRoutes />
-      </AuthProvider>
-    </BrowserRouter>
+    <App />
   </React.StrictMode>
-)
+);
